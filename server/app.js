2da3dca80c178221683f05944c7603d0c04b1c6d
Ronaldo <ronaldosurya1507@gmail.com>
2023-12-12 21:26:52 +0700

    Finish Server

--- server/app.js
+++ server/app.js
@@ -1,7 +1,11 @@
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+}
+
 const express = require('express')
 const cors = require('cors');
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const { User , Chart} = require('./models');
 const { comparePassword } = require('./helpers/bcrypt');
 const { signToken } = require('./helpers/jwt');
